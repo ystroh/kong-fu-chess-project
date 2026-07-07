101b45449509ced7f63421a9e1aa12d1556b74bc
ystroh <y0534178779@example.com>
2026-07-07 19:58:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kong-fu-chess-project
---
+++ kong-fu-chess/Classes/Main.java
@@ -0,0 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
