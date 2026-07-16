7ae513714bfa5fc3ecf9fd7524be6eca32c79f40
ystroh <y0534178779@example.com>
2026-07-16 14:53:17 +0300

    add link to git

--- kong-fu-chess/src/main/java/com/chessgame/Main.java
+++ kong-fu-chess/src/main/java/com/chessgame/Main.java
@@ -1,5 +1,6 @@
 package com.chessgame;
-
+//קישור לגיט
+//https://github.com/ystroh/kong-fu-chess-project.git
 public class Main {
     public static void main(String[] args) {
         new App().run();
